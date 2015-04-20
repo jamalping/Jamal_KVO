9c756911de487492f30bcaec3e5e313ce965d6c3
jamalping <jamalping@shenji.workgroup>
2015-04-20 15:46:30 +0800

    Initial Commit

---
+++ JamalKVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JamalKVO
+//
+//  Created by jamalping on 15/4/20.
+//  Copyright (c) 2015年 李小平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JamalKVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JamalKVO
+//
+//  Created by jamalping on 15/4/20.
+//  Copyright (c) 2015年 李小平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
